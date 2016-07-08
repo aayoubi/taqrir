1a44da37b5317fef2b5183398297fcbaa474852c
Ali AYOUBI <aloush89@gmail.com>
2016-07-08 19:29:39 +0300

    Fix React class hierarchy

--- js/dataviz-react.js
+++ js/dataviz-react.js
@@ -1,18 +1,14 @@
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-    // Great success! All the File APIs are supported.
-} else {
-    alert('The File APIs are not fully supported in this browser.');
-}
-
 // React Class Hierarchy
 //  - MxTimeTable
 //      - FileSelector
-//      - ActivityTables
-//          - TeamTable
-//          - UserTables
-//              - Table
-//              - Table
-//              - Table
+//      - TeamTable
+//        - PieChart
+//        - BarChart
+//      - UserTables
+//        - User
+//          - PieChart
+//          - BarChart
+//        ...
 
 var MxTimeTable = React.createClass({
   getInitialState: function() {
