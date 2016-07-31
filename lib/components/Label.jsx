478794fa6aed2489e69c7db85e0438302dcb20c7
Ali AYOUBI <aloush89@gmail.com>
2016-08-01 00:43:13 +0200

    Fix css className

--- lib/components/Label.jsx
+++ lib/components/Label.jsx
@@ -4,7 +4,7 @@ define(function (require) {
 	const Label = React.createClass({
 	    render: function() {
 	      return (
-	        <div className="label">
+	        <div className="desc">
 	          <h3>{this.props.title}</h3>
 	          <p>spent {this.props.manDays.toFixed(2)} days working.</p>
 	        </div>
